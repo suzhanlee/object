2150c3af7bd12be35b6f7dd65c6e44ca6fede230
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-06 19:31:00 +0900

    Object chapter 13 : 서브클래싱과 서브타이핑

---
+++ src/main/java/com/object/domain/chapter13/Client.java
@@ -0,0 +1,18 @@
+package com.object.domain.chapter13;
+
+public class Client {
+
+    public void resize(Rectangle rectangle, int width, int height) {
+        rectangle.setWidth(width);
+        rectangle.setHeight(height);
+        assert rectangle.getWidth() == width && rectangle.getHeight() == height;
+    }
+
+    public static void main(String[] args) {
+        Square square = new Square(10, 10, 10);
+        Client client = new Client();
+        client.resize(square, 50, 100);
+        System.out.println(square.getHeight());
+        System.out.println(square.getWidth());
+    }
+}
